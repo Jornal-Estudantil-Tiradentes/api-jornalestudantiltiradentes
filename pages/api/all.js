b54556fbd84a73ace37ced984521bebdc50a1cdb
marshmll <renandasilvaoliveiraandrade@gmail.com>
2023-05-14 14:23:01 -0300

    Criação rotas /item/[id]; /list/[type], /all

---
+++ pages/api/all.js
@@ -0,0 +1,12 @@
+import { buildClient } from '@datocms/cma-client-node';
+
+export default async function news(req, res) {
+  const DATOCMS_TOKEN = process.env.DATOCMS_TOKEN;
+  const client = buildClient({ apiToken: DATOCMS_TOKEN });
+
+  const records = await client.items.list();
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Cache-Control', 's-maxage=15, stale-while-revalidate');
+  res.json(records);
+}
---
+++ pages/api/list/[type].js
@@ -0,0 +1,18 @@
+import { buildClient } from '@datocms/cma-client-node';
+
+export default async function news(req, res) {
+  const DATOCMS_TOKEN = process.env.DATOCMS_TOKEN;
+  const client = buildClient({ apiToken: DATOCMS_TOKEN });
+
+  const type = req.query.type;
+
+  const records = await client.items.list({
+    filter: {
+      type: type
+    },
+  });
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Cache-Control', 's-maxage=15, stale-while-revalidate');
+  res.json(records);
+}
