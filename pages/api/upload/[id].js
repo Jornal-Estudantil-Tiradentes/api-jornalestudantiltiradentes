f13e4d9e525d4019c094abe9ae2ace79ca1162f5
marshmll <renandasilvaoliveiraandrade@gmail.com>
2023-06-04 17:58:37 -0300

    Formatação com Prettier, correação de res.send() para res.json()

--- pages/api/upload/[id].js
+++ pages/api/upload/[id].js
@@ -1,4 +1,4 @@
-import { buildClient } from '@datocms/cma-client-node';
+import { buildClient } from "@datocms/cma-client-node";
 
 export default async function news(req, res) {
   const DATOCMS_TOKEN = process.env.DATOCMS_TOKEN;
@@ -8,7 +8,7 @@ export default async function news(req, res) {
 
   const upload = await client.uploads.find(id);
 
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Cache-Control', 's-maxage=15, stale-while-revalidate');
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Cache-Control", "s-maxage=15, stale-while-revalidate");
   res.json(upload);
 }
